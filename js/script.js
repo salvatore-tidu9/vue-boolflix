336773bdd49489c5557f0634769adf031af42bbf
Salvatore Tidu <salvatoret9@outlook.it>
2021-04-07 21:32:25 +0200

    Vue_Boolflix_(Milestone_2 + Milestone_3 + Milestone_4)

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ var app = new Vue({
 
     data: {
 
-        filmsList: [],
+        moviesList: [],
+
+        tvShowsList: [],
 
         searchText: "",
+
+        flagsList: ["be", "bg", "ca", "cn", "de", "en", "es", "fr", "gr", "hr", "ie", "il", "it", "jp", "kr", "nl", "pl", "pt", "ru", "tr"]
 
     },
 
@@ -18,18 +22,63 @@ var app = new Vue({
 
                 axios
 
-                .get("https://api.themoviedb.org/3/search/movie?api_key=3410f7b084766853ff2eead521a6391b&query=" + this.searchText)
+                .get("https://api.themoviedb.org/3/search/movie",
+
+                    {
+                        params: {
+
+                            api_key: "3410f7b084766853ff2eead521a6391b",
+
+                            query: this.searchText,
+
+                            language: 'it-IT'
+                        }
+                    }
+                )
 
                 .then((response) => {
 
-                    this.filmsList = response.data.results;
+                    this.moviesList = response.data.results;
 
-                    this.searchText = "";
+                });
+
+                axios 
+
+                .get("https://api.themoviedb.org/3/search/tv",
+
+                    {
+                        params: {
+
+                            api_key: "3410f7b084766853ff2eead521a6391b",
+
+                            query: this.searchText,
+
+                            language: 'it-IT'
+                        }
+                    }
+                )
+
+                .then((response) => {
+
+                    this.tvShowsList = response.data.results;
 
                 });
 
             }
+
+            this.searchText = "";
+        },
+
+        voteStars(number) {
+
+            return Math.ceil(number / 2);
+        },
+
+        getFlag(index) {
+            
+            return this.flagsList.includes(index);
         }
+
     }
 
 });
